3efc59669e1ef3cfa1ca2953f65a4276744bc58c
Alex Scrobot <scrobot91@gmail.com>
2020-05-14 14:47:48 +0700

    TSKAD-209: implemented multiply consuming for topics

--- receiver/src/main/java/com/scrobot/receiver/ThinTopicsConsumer.kt
+++ receiver/src/main/java/com/scrobot/receiver/ThinTopicsConsumer.kt
@@ -1,22 +1,19 @@
 package com.scrobot.receiver
 
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
-class FatTopicConsumer(
+class ThinTopicsConsumer(
+    private val json: Json,
     private val repository: EventRepository
 ) {
 
-    private val logger: Logger = LoggerFactory.getLogger(FatTopicConsumer::class.java)
+    private val logger = LoggerFactory.getLogger(ThinTopicsConsumer::class.java)
 
-    private val json = Json(JsonConfiguration.Stable)
-
-    @KafkaListener(topics = ["fat_topic"], groupId = "fat_topic_consumers")
+    @KafkaListener(topicPattern = "thin_topic_*", groupId = "thin_topic_consumers")
     fun consume(message: String?) = message?.let {
         json.parse(Event.serializer(), it)
     }?.also {
